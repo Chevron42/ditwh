31c799763949c0d1d64e9c7c8fabb9d3b95f75bb
alicem <alice.mottola@gmail.com>
2013-08-29 18:21:14 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ditwh::Application.config.secret_token = 'd48e39ea9afc4ed615e0e8968f90b295472da3a0d1ee3910c5cd431d6821ca657e448da5d3c3cbbac41794deab7f7369ba68dab8a1351dd98099292bd2a1f01b'
