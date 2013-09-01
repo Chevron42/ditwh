2c8340547acf8cb17131f920a621c5608d47e101
alicem <alice.mottola@gmail.com>
2013-09-01 17:32:39 -0400

    Working on player

---
+++ app/assets/javascripts/game.js
@@ -0,0 +1,48 @@
+var Game = {
+  display: null,
+  map: [],
+  MAP_WIDTH: 141,
+  MAP_HEIGHT: 41,
+  player: null,
+
+  init: function() {
+    this.display = new ROT.Display({ width: this.MAP_WIDTH, height: this.MAP_HEIGHT, fontSize: 13 });
+    document.body.appendChild(this.display.getContainer());
+    this._generateMap();
+
+    var scheduler = new ROT.Scheduler.Simple();
+    scheduler.add(this.player, true);
+    this.engine = new ROT.Engine(scheduler);
+    debugger;
+    this.engine.start();
+  },
+
+  _generateMap: function() {
+
+    var arkham = new ROT.Map.Arkham(this.MAP_WIDTH, this. MAP_HEIGHT, window.map);
+
+    // map.create(this.arkhamCallback.bind(this));
+    this._drawWholeMap(arkham);
+
+    this._createPlayer();
+
+  },
+
+  arkhamCallback: function(x, y, value) {
+    this.map[i][j] = value;
+  },
+
+  _drawWholeMap: function(map) {
+    for (var x=0; x<this.MAP_WIDTH; x+=1) {
+      for (var y=0; y<this.MAP_HEIGHT; y+=1) {
+        this.display.draw(x, y, map._map[y][x]);
+      }
+    }
+  }, // end drawWholeMap
+
+  _createPlayer: function() {
+    // MAKE THESE PARAMS DYNAMIC
+    this.player = new Player(71, 21);
+  }
+
+};
---
+++ app/assets/javascripts/player.js
@@ -0,0 +1,44 @@
+//= require rot
+//= require_ tree .
+
+var Player = function(x, y) {
+    this._x = x;
+    this._y = y;
+    this._draw();
+};
+
+Player.prototype._draw = function() {
+  Game.display.draw(this._x, this._y, "@", "#fff");
+};
+
+Player.prototype.act = function() {
+  Game.engine.lock();
+  window.addEventListener("keydown", this);
+};
+
+Player.prototype.handleEvent = function(e) {
+    var keyMap = {};
+    keyMap[38] = 0;
+    keyMap[39] = 2;
+    keyMap[40] = 4;
+    keyMap[37] = 6;
+
+    var code = e.keyCode;
+    /* one of numpad directions? */
+    if (!(code in keyMap)) { return; }
+
+    /* is there a free space? */
+    var dir = ROT.DIRS[8][keyMap[code]];
+    var newX = this._x + dir[0];
+    var newY = this._y + dir[1];
+    var newKey = newX + "," + newY;
+    if (!(newKey in Game.map)) { return; }
+
+    Game.display.draw(this._x, this._y, Game.map[this._x+","+this._y]);
+    this._x = newX;
+    this._y = newY;
+    this._draw();
+
+    window.removeEventListener("keydown", this);
+    Game.engine.unlock();
+};
