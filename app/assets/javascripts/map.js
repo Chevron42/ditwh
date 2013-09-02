ed3520f444edfc86f5b4da04785721f737125232
alicem <alice.mottola@gmail.com>
2013-09-02 11:20:41 -0400

    Adds traps to three sectors

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -51,24 +51,24 @@ ROT.Map.Arkham = function (width, height, anEmptyMap) {
   };
 
   this.ARKHAM_SQ = {
-    upperLeft: [93, 0],
-    upperRight: [140, 0],
-    lowerLeft: [93, 140],
-    lowerRight: [140, 140],
+    upperLeft: [94, 0],
+    upperRight: [130, 0],
+    lowerLeft: [94, 130],
+    lowerRight: [130, 130],
     traps: [this.TILE.EAST_TRAP_1, this.TILE.EAST_TRAP_2]
   };
 
   this.MEADOW_HILL = {
-    upperLeft: [47, 125],
+    upperLeft: [16, 125],
     upperRight: [92, 125],
-    lowerLeft: [47, 140],
-    lowerRight: [92, 140],
+    lowerLeft: [16, 131],
+    lowerRight: [92, 131],
     traps: [this.TILE.SOUTH_TRAP_1, this.TILE.SOUTH_TRAP_2]
   };
 
   this.FOREST = {
-    upperLeft: [47, 0],
-    upperRight: [92, 0],
+    upperLeft: [47, 7],
+    upperRight: [92, 7],
     lowerLeft: [47, 14],
     lowerRight: [92, 14],
     traps: [this.TILE.NORTH_TRAP_1, this.TILE.NORTH_TRAP_2]
@@ -81,60 +81,38 @@ ROT.Map.Arkham = function (width, height, anEmptyMap) {
     lowerRight: [92, 124]
   };
 
-  this.SECTORS = [this.MISKATONIC_U, this.ARKHAM_SQ, this.FOREST, this.MEADOW_HILL];
+  this.SECTORS = [this.MISKATONIC_U, this.ARKHAM_SQ, this.MEADOW_HILL, this.FOREST];
 
   return this;
 };
 
 ROT.Map.Arkham.extend(ROT.Map);
 
-ROT.Map.Arkham.prototype.create = function () {
-
-  //debugger;
-  // generate map for each sector
-  // var sectors_length = this.SECTORS.length,
-  //   i,
-  //   j,
-  //   k;
-  // for (i = 0; i < sectors_length; i += 1) {
-  //   var sector = this.SECTORS[i];
-  //   var sector_upper_right = sector.upperRight[0];
-  //   for (j = sector.upperLeft[0]; j < sector_upper_right; i += 1) {
-  //     var sector_lower_left = sector.lowerLeft[1];
-  //     // for (k = sector.upperLeft[1]; k < sector_lower_left; k += 1) {
-  //     //   var rand = ROT.RNG.getUniform();
-  //     //   var tile;
-  //     //   if (rand < 0.39) {
-  //     //     tile = sector.traps[0];
-  //     //   } else if (rand >= 0.39 && rand < 0.79) {
-  //     //     tile = sector.traps[1];
-  //     //   } else {
-  //     //     tile = this.TILE.SAFE;
-  //     //   }
-  //     //   debugger;
-  //     //   this._map[j][k] = tile;
-  //     // }
-  //   }
-  // }
+ROT.Map.Arkham.prototype.create = function() {
 
   for (var s = 0; s < this.SECTORS.length; s += 1) {
-    var startWidth = this.MISKATONIC_U.upperLeft[0];
-    var endWidth = this.MISKATONIC_U.upperRight[0];
-    var startHeight = this.MISKATONIC_U.upperLeft[1];
-    var endHeight = this.MISKATONIC_U.lowerRight[1];
+    var sector = this.SECTORS[s];
+    var startWidth = sector.upperLeft[0];
+    var endWidth = sector.upperRight[0];
+    var startHeight = sector.upperLeft[1];
+    var endHeight = sector.lowerRight[1];
     var rand;
+    var tile;
     for (var i = startWidth; i < endWidth; i += 1) {
-    for (var j = startHeight; j < endHeight; j += 1) {
-      if (this._map[i][j] !== ' ') {
-        rand = ROT.RNG.getUniform();
-        if (rand < 0.29) { this._map[i][j] = this.TILE.WEST_TRAP_1; }
-        else if (rand < 0.59) { this._map[i][j] = this.TILE.WEST_TRAP_2; }
-        else { this._map[i][j] = this.TILE.SAFE; }
+      for (var j = startHeight; j < endHeight; j += 1) {
+        if (this._map[i][j] !== ' ') {
+
+          rand = ROT.RNG.getUniform();
+          if (rand < 0.19) { tile = sector.traps[0]; }
+          else if (rand < 0.39) { tile = sector.traps[1]; }
+          else { tile = this.TILE.SAFE; }
+
+          this._map[i][j] = tile;
+
+        }
       }
     }
   }
 
-  }
-  //debugger;
   return this._map;
 };
